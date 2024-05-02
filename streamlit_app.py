64a49a5a3a1b21243092ea53b60bde75d7c64fdb
Joe <jooe3005@gmail.com>
2024-05-03 00:46:50 +0200

    Rajout fichier

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,40 @@
 ﻿import streamlit as st
 import requests
 
+def format_model_name(full_name):
+    # Extraire le nom après le '/' et prendre le premier mot
+    short_name = full_name.split('/')[1].split('-')[0].capitalize()  # Capitalize pour améliorer l'affichage
+    return short_name
+
+# Liste des modèles avec leurs noms complets
+model_names = [
+    "Dofla/distilbert-squad", 
+    "Dofla/roberta-base", 
+    "csarron/bert-base-uncased-squad-v1"
+]
+
+# Préparation de la liste pour l'interface
+model_options = [(name, format_model_name(name)) for name in model_names]
+
 def main():
-    st.title("VIT'IA 2.0")
+    st.title("Question Answering System")
     
-    # Utilisateur peut entrer un contexte manuellement ou charger un fichier
-    context = st.text_area("Enter the text context here (optional if you upload a file):", height=200)
-    uploaded_file = st.file_uploader("Or upload a text file with context", type=['txt'], key="file_uploader")
+    # Liste de modèles formatée pour l'affichage et la sélection
+    model_options = [
+        ("Dofla/distilbert-squad", "Distilbert"),
+        ("Dofla/roberta-base", "Roberta"),
+        ("csarron/bert-base-uncased-squad-v1", "Bert")
+    ]
 
-    # Si un fichier est téléchargé, utilisez ce contexte
-    if uploaded_file is not None:
-        file_context = str(uploaded_file.read(), 'utf-8')
-        context = file_context  # Utiliser le contenu du fichier comme contexte
-        st.write("Uploaded File Context (first 500 chars):", context[:500])
+    # Permettre aux utilisateurs de choisir le modèle
+    model_choice = st.selectbox(
+        "Choose the model you want to use:",
+        options=model_options,
+        index=0,  # par défaut, sélectionnez le premier modèle
+        format_func=lambda x: x[1]  # Utilisez le deuxième élément des tuples pour l'affichage
+    )[0]  # Prendre le premier élément pour l'usage
 
-    question = st.text_input("Enter your question:", key="question_input")
-
-    # Un seul bouton pour obtenir la réponse
-    if st.button("Get Answer", key="get_answer_button"):
-        if context and question:
-            # Envoyer la requête au backend
-            response = requests.post("http://localhost:8000/answer/", json={"context": context, "question": question})
-            if response.status_code == 200:
-                answer = response.json().get('answer')
-                st.write("Answer:", answer)
-            else:
-                st.error("Failed to get answer from the server.")
-        else:
-            st.error("Please provide a context and enter a question.")
+    # Suite de votre code...
 
 if __name__ == "__main__":
     main()
